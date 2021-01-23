4acf6cad88927f05635c3ffec5273b2419a76846
Ali <alastair.r.mcneill@gmail.com>
2021-01-23 19:34:48 +0000

    Spawns new enemies

--- Components/Low_enemy.py
+++ Components/Low_enemy.py
@@ -2,8 +2,8 @@ import pygame
 from Components.Enemy import Enemy
 from Components.Constants import PLAYER_IMGS
 
-class High_enemy(Enemy):
+class Low_enemy(Enemy):
     def __init__(self):
         super().__init__()
-        self.y = 200
+        self.y = 400
         self.IMGS = PLAYER_IMGS
--- Scenes/Game.py
+++ Scenes/Game.py
@@ -1,22 +1,30 @@
 import pygame
+import random
 
 from Scenes.Scene import Scene
 from Components.Player import Player
 from Components.High_enemy import High_enemy
+from Components.Low_enemy import Low_enemy
 from Components.Constants import BG_IMG
 
 class Game(Scene):
     def __init__(self):
         super().__init__()
+        self.reset()
+
+    def reset(self):
         self.img = BG_IMG.convert()
         self.vel = 2
         self.x1 = 0
         self.x2 = self.img.get_width()
         self.player = Player()
         self.enemies = [High_enemy()]
+        self.enemy_timer = 0
+        self.min_gap = 50
+        self.range_gap = 50
 
     def startup(self, persist):
-        pass
+        self.reset()
 
     def cleanup(self):
         self.done = False
@@ -41,12 +49,18 @@ class Game(Scene):
         if self.x2 + self.img.get_width() <0:
             self.x2 = self.x1 + self.img.get_width()
 
+        self.add_enemy()
+
         self.player.move()
 
         for en in self.enemies:
             en.move()
             if en.check_collision(self.player):
                 print("Crashed")
+                self.done = True
+                self.next = "Game"
+            if en.x + en.current_img.get_width() < 0:
+                self.enemies.pop(self.enemies.index(en))
 
 
 
@@ -56,3 +70,14 @@ class Game(Scene):
         self.player.draw(screen)
         for en in self.enemies:
             en.draw(screen)
+
+
+    def add_enemy(self):
+        self.enemy_timer += 1
+        if self.enemy_timer > self.min_gap + 100 * random.randint(0,self.range_gap):
+            if random.random() > 0.5:
+                self.enemies.append(High_enemy())
+            else:
+                self.enemies.append(Low_enemy())
+            self.enemy_timer = 0
+
